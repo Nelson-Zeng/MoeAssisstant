de042d68d39946b5e5f3a577e73bbe3da617bcb5
平胸宁海异闻录 <18827600638m@sina.cn>
2020-02-21 08:54:15 +0800

    1.提取自定义表格组件方便复用 2.实现舰船图鉴页面查看掉落的功能

---
+++ moe-assisstant/components/custom-table/custom-table.js
@@ -0,0 +1,18 @@
+Component({
+  properties: {
+    tableContent: {
+      type: Object,
+      value: {}
+    },
+    widthDistribution: {
+      type: Object,
+      value: []
+    }
+  },
+  data: {
+
+  },
+  methods: {
+
+  }
+})
